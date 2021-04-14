1339b71afa9d2992f785b6d3719b461dcfb9841f
caniksea <caniksea@yahoo.co.nz>
2021-04-14 18:34:55 +0200

    Implement mine

---
+++ src/main/java/ac/za/cput/demo/Account.java
@@ -0,0 +1,49 @@
+package ac.za.cput.demo;
+
+import java.util.Objects;
+
+public class Account {
+
+    private String id, name, type;
+
+    public Account(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return id.equals(account.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/test/java/ac/za/cput/demo/AccountTest.java
@@ -0,0 +1,16 @@
+package ac.za.cput.demo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    @Disabled
+    void testIgnore() {
+        System.out.println("You are ignored!");
+    }
+
+}
